68e9228be39056006e776ebbd7acefe8c7393969
Shashankpandey09 <137607589+Shashankpandey09@users.noreply.github.com>
2024-03-29 16:54:48 -0800

    second commit

--- src/Components/Advertisement.tsx
+++ src/Components/Advertisement.tsx
@@ -7,15 +7,15 @@ interface props{
 
 const Advertisement:React.FC<props> = ({imageUrl,title}) => {
   return (
-    <div className="flex justify-between flex-col items-center  h-full pt-2 shadow-lg px-2" >
-    <div className="w-[217px]">
+    <div className="flex justify-between flex-col items-center h-full  pt-2 px-2" >
+    <div className="w-[217px] h-[97%] border border-[#006555]">
       <div className='image relative   '>
         <img src={imageUrl} alt="advertisement " className=" object-cover" />
         <button className="w-[47.78px] absolute top-0 right-0 bg-black text-white " >Ad</button>
       </div>
       <div className="mt-4  text-center">
         <p className="font-semibold text-[20px]">Advertisement title</p>
-      <h2 className="text-[12.8px] self-start text-start line-height-1.5 py-1">{title}</h2>
+      <h2 className="text-[12.8px] text-justify line-height-2 px-3 py-1">{title}</h2>
     </div> 
     </div>
   </div>
